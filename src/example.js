84d6f5e2eaaf5f90e5bc513f16029b0957e71ecd
mar1n <30738216+mar1n@users.noreply.github.com>
2020-03-31 19:55:06 +0100

    build promise part 3 async await

--- src/example.js
+++ src/example.js
@@ -35,4 +35,11 @@ function asyncAdd(values) {
 }
 
 let values = [10, 20, 30, 40, 50];
-asyncAdd(values).then(total => console.log(`Main Total ${total}`));
+//asyncAdd(values).then(total => console.log(`Main Total ${total}`));
+
+async function doTask() {
+    let total = await asyncAdd(values);
+    console.log(`Main Total: ${total}`);
+}
+
+doTask();
